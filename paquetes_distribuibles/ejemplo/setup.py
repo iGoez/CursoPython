0dca0c663ffd5347e9f7e29ef436e2233f97ef21
Camilo Goez <camilogoez2015@hotmail.com>
2020-12-27 13:36:07 -0500

    Agregando ejemplo de paquetes distribuidos

---
+++ paquetes_distribuibles/ejemplo/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="calculos",
+      version="1.0",
+      description="Paquete de cálculos matemáticos",
+      author="",
+      author_email="",
+      url="",
+      packages=["calculos"])
