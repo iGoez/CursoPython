aea4f442cbda2d38bc45266b7ae02715d452fde2
Camilo Goez <camilogoez2015@hotmail.com>
2020-12-26 21:49:39 -0500

    Algunas instrucciones que se puede itulizarn en String

---
+++ metodos_cadena/metodosCadenas.py
@@ -0,0 +1,12 @@
+# upper() todas las letras en mayúsculas
+# lower() todas las letras en minúsculas
+# capitalize() Primera letra en mayúscula
+# count() contar cuantas letras aparecen dentro de una cadena
+# find() representa el índice donde aparece un carácter dentro de un texto
+# isdigit() devuelve un booleano si es un valor numérico o no
+# isalum() comprueba si es alfanuméricos
+# isalpha() comprueba si son solo letras
+# split()  Crea una lista con las palabras separadas
+# strip() borra los espacios sobrante al principio y al final
+# replace() cambia una palabra o una letra por otra
+# rfind() representa el índice de un carácter contando desde atrás
